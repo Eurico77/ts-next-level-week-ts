fb53d92807742cad98d27ed91a44011356cc6dc5
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-24 23:02:18 -0300

    add repositorie user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,16 +1,32 @@
 import { Request, Response } from 'express'
+import { getCustomRepository } from 'typeorm'
+import { UsersRepository } from '../repositories/UsersRepository'
 
 class UserController {
-    
     static async create(request: Request, response: Response) {
 
-        const user = request.body
-         console.log(user);
-         return response.json({user})
-        
+        const { name, email } = request.body
+        const userRepository = getCustomRepository(UsersRepository)
+
+        const userAlreadyExists = await userRepository.findOne({
+            email
+        })
+
+        if (userAlreadyExists) {
+            return response.status(400).json({
+                message: 'User already exists'
+            })
+        }
+
+        const user = userRepository.create({
+            name, email
+        })
+
+        await userRepository.save(user)
+
+        return response.status(201).json(user)
+
     }
-
-
 
 }
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -5,7 +5,6 @@ import { v4 as uuid } from 'uuid'
 @Entity('users')
 class User {
 
-
     @PrimaryColumn()
     readonly id: string;
 
