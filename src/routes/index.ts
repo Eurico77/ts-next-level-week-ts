492404880fd992f2a5b182288ef5875b828ffc0f
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-24 23:27:49 -0300

    refatorando

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
-import userRoutes from './userRoutes'
+
+import { userRoutes } from './userRoutes'
+import { surveyRoutes } from './surveyRoutes'
 
 const router = Router()
 
 router.use('/users', userRoutes)
+router.use('/surveys', surveyRoutes)
+
 
 export default router
