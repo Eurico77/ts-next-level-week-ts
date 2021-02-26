698f2328909f820fb5fa6e23e47fd74f98a185ed
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-26 13:09:27 -0300

    refactor teste

--- __tests__/User.test.ts
+++ __tests__/User.test.ts
@@ -5,22 +5,31 @@ import createConnection from '../src/database/connection'
 
 
 
+
 describe('Users', () => {
-
+    let connection = null
+   
     beforeAll(async () => {
-        const connection = await createConnection();
- 
-        
-
+        connection = await createConnection();
         await connection.runMigrations();
 
     })
+
+    beforeEach(async () => {
+        await connection.transaction
+        
+    })
+
+    afterAll(async () => {
+        await connection.close()
+    })
+
 
     it('Should be able to create a new user', async () => {
         const response = await request(app).post('/users')
             .send({
                 name: 'user name',
-                email: 'user@.comsad',
+                email: 'user@.cosadsmsad',
             })
 
         expect(response.status).toBe(201)
