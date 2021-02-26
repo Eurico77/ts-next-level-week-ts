f8f658b2c0103789e55ae9c7ce6570b9f9e9c426
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-26 13:08:40 -0300

    refactor migrate

--- src/migrations/1614119050984-CreateUsers.ts
+++ src/migrations/1614119050984-CreateUsers.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 export class CreateUsers1614119050984 implements MigrationInterface {
 
     public async up( queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+       
         await queryRunner.createTable(
             new Table({
                 name: "users",
