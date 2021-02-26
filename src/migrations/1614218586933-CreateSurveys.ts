e383edc703a0b594134959fbad7aee0bb6820996
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-26 13:08:59 -0300

    refactor migrate

--- src/migrations/1614218586933-CreateSurveys.ts
+++ src/migrations/1614218586933-CreateSurveys.ts
@@ -4,6 +4,7 @@ export class CreateSurveys1614218586933 implements MigrationInterface {
 
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
         await queryRunner.createTable(
             new Table({
                 name: "surveys",
